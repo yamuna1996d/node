a4615a9253d3d01b51573313b9d91b9bbac0bec6
yamuna1996d <yamunag19@gmail.com>
2020-04-01 10:41:28 +0530

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var ex=require('express');
 var parse=require('body-parser');
 var host=ex();
 host.use(parse.urlencoded({extended:false}));
-host.post('/odd',(req,res)=>{
+host.get('/odd',(req,res)=>{
     var n=200;
     var i;
     for (i = 1; i <= n; i++) {
